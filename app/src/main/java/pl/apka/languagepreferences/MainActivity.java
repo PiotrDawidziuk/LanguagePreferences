abdf1d5e684e518dfd5950755a6ffde185e263d6
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-01-23 23:54:14 +0100

    alert dialog added

--- app/src/main/java/pl/apka/languagepreferences/MainActivity.java
+++ app/src/main/java/pl/apka/languagepreferences/MainActivity.java
@@ -1,13 +1,40 @@
 package pl.apka.languagepreferences;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void setLanguage (String language){
+        
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_btn_speak_now)
+                .setTitle("Choose Language")
+                .setMessage("Which language do you want to use")
+                .setPositiveButton("English", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //set english
+                    }
+                })
+                .setNegativeButton("Spanish", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //set spanish
+                    }
+                })
+                .show();
+
+
     }
 }
